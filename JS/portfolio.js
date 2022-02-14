53b98ffe7f09c363f1c5f01adb5d557449f9f0ba
salxvador <sal.salvador@gmail.com>
2022-02-14 08:59:03 -0500

    added slideshow and pop-up contact form.

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -28,14 +28,32 @@ closeForm = () => {
     document.getElementById("myForm").style.display = "none";
 }
 
+// This function displays the first image in the slideshow when the page loads
+var slideIndex = 1;
+showSlides(slideIndex);
 
-$(".slideshow > div:gt(0)").hide();
+// This function changes the slide when the left or right arrows are clicked
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
-setInterval(function() {
-    $(".slideshow > div:first")
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo(".slideshow");
-}, 3000);
+// This function changes the slide when the dots are clicked
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+
+function showSlides(n) {
+    var slides = document.getElementsByClassName("slide"); // This takes all slideshow elements and stores them in the variable array
+    var dots = document.getElementsByClassName("dot"); // Sore all dots in an array
+    if (n > slides.length) {slideIndex = 1}; // this is needed so that the slide numbers will loop around the array
+    if (n < 1) {slideIndex = slides.length};
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; // This for loop takes each item in the array and sets the display to none (default)
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", ""); // This for loop takes each item in the dots array and removes "active"
+    }
+    slides[slideIndex - 1].style.display = "block"; // displays the image
+    dots[slideIndex - 1].className += " active"; // add the active styling to the dot for the image
+}
